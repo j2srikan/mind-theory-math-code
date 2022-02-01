945bb6bd06fec4c5b661c1a6222c724b95f7adac
Sixuan Chen <sixuanchen@Sixuans-MacBook-Pro.local>
2022-02-01 12:38:37 -0500

    busy beaver 5 state

--- code/prog-lang-prelim/racket/greeting.rkt
+++ code/prog-lang-prelim/racket/greeting.rkt
@@ -3,3 +3,19 @@
   (print "hello world!"))
 
 (greeting)
+
+(struct cell ([content #:mutable]) #:transparent)
+(define a-cell (cell 0))
+(set-cell-content! a-cell 1)
+
+
+
+
+(define (factorial n)
+    (do ((counter n (- counter 1))
+        (result 1 (* result counter)))
+    ((= counter 0) result) ; Stop condition and return value.
+    ; The body of the do-loop is empty.
+    ))
+
+(factorial 5)
--- code/prog-lang-prelim/racket/twice.rkt
+++ code/prog-lang-prelim/racket/twice.rkt
@@ -13,3 +13,11 @@
 ; 16/2 = 8, 8/2 =4
 (twice divide 16)
 
+(define counter 6)
+(define factorial 1)
+(let loop ()
+    (set! factorial (* factorial counter))
+    (set! counter (sub1 counter))
+    (displayln factorial)
+    (when (> counter 0) (loop)))
+(displayln factorial)
